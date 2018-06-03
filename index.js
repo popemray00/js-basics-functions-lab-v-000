a2d5f8463bb02eaf5361ed7647ffc0c6a688977c
Michael Pope <popemray1986@gmail.com>
2018-06-03 19:34:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 // Code your solution in this file!
 function distanceFromHqInBlocks(someValue) {
  if (someValue > 42) {
-   return someValue - 42; 
+   return someValue - 42;
  }
  else {
    return 42 - someValue
  }
+}
+
+function distanceFromHqInFeet(someValue) {
+  return distanceFromHqInBlocks(someValue) * 264
 }
